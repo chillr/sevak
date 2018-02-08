15e23767c426a3a290168f7cf50df47712868df0
kajol <maheshwari.kajol@gmail.com>
2018-02-08 11:38:39 +0530

    Adding method for delayed publishing

--- lib/sevak/publisher.rb
+++ lib/sevak/publisher.rb
@@ -6,11 +6,28 @@ module Sevak
     include Singleton
 
     def self.publish(queue_name, message)
-      instance.channel.queue(queue_name).publish(message.to_json)
+      instance.queue(queue_name).publish(message.to_json)
+    end
+
+    def self.delayed_publish(queue_name, message, delay = 0)
+      instance.publish_exchange(queue_name, message, delay)
     end
 
     def channel
       @channel ||= connection.create_channel
+    end
+
+    def queue(queue_name)
+      @queue ||= channel.queue(queue_name)
+    end
+
+    def exchange(queue_name)
+      @exchange ||= channel.exchange("#{queue_name}_exchange", type: "x-delayed-message", arguments: { "x-delayed-type" => "direct" })
+    end
+
+    def publish_exchange(queue_name, message, delay)
+      queue(queue_name).bind(exchange(queue_name))
+      exchange(queue_name).publish(message.to_json, headers: { "x-delay" => delay })
     end
   end
 end
--- test/sevak/publisher_spec.rb
+++ test/sevak/publisher_spec.rb
@@ -8,14 +8,66 @@ module Sevak
         f.host = 'localhost'
         f.port = '5672'
       end
+      @pub = Publisher.send(:new)
+      @queue_name = 'sms'
     end
 
     it 'should have a publish method' do
       assert_equal true, Publisher.respond_to?(:publish)
     end
 
-    it 'should publish message to the queue specified' do
-      Publisher.publish('sms', { msisdn: '+919894321290', message: 'Testing the message publish' })
+    it 'should have a delayed_publish method' do
+      assert_equal true, Publisher.respond_to?(:delayed_publish)
     end
+
+    describe '#channel' do
+      it 'should create a channel' do
+        @pub.channel
+        refute_nil @pub.instance_variable_get(:@channel)
+      end
+    end
+
+    describe '#queue' do
+      it 'should create a queue with the given name' do
+        @pub.queue(@queue_name)
+        refute_nil @pub.instance_variable_get(:@queue)
+        assert_equal @pub.instance_variable_get(:@queue).name, @queue_name
+      end
+    end
+
+    describe '#publish' do
+      it 'should publish message to the queue specified' do
+        Publisher.publish(@queue_name, { msisdn: '+919894321290', message: 'Testing the message publish' })
+      end
+    end
+
+    describe '#exchange' do
+      before do
+        @pub.exchange('sms')
+      end
+
+      it 'should create an exchange with name #{@queue_name}_exchange specified' do
+        refute_nil @pub.instance_variable_get(:@exchange)
+        assert_equal @pub.instance_variable_get(:@exchange).name, "#{@queue_name}_exchange"
+      end
+
+      it 'should create an exchange with type x-delayed-message' do
+        assert_equal @pub.instance_variable_get(:@exchange).type, "x-delayed-message"
+      end
+    end
+
+    describe '#delayed_publish' do
+      before do
+        @pub.queue(@queue_name).purge
+        Publisher.delayed_publish(@queue_name, { msisdn: '+919894321290', message: 'Testing the delayed message publish' }, 10000)
+      end
+
+      it 'should route messages from the exchange to the queue' do
+        assert_equal 0, @pub.queue(@queue_name).message_count
+        sleep 20
+        assert_equal 1, @pub.queue(@queue_name).message_count
+      end
+    end
+
   end
 end
