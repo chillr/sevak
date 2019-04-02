5618414e4c820d86760dad64610f4c87a5cd5bb7
bibi <bibi.da@chillr.in>
2019-04-02 10:51:01 +0530

    adding attempt counter

--- lib/sevak/core.rb
+++ lib/sevak/core.rb
@@ -1,21 +1,25 @@
 module Sevak
   module Core
     def connection
+      attempt = 0
       begin
-        return @conn if @conn
-        host = config.host.kind_of?(Array) ? config.host : [config.host]
-        @conn ||= Bunny.new(
-          hosts: host,
-          port: config.port,
-          username: config.user,
-          password: config.password)
-        @conn.start
-        @conn
+        if attempt < 10
+          host = config.host.kind_of?(Array) ? config.host : [config.host]
+          @conn = Bunny.new(
+            hosts: host,
+            port: config.port,
+            username: config.user,
+            password: config.password)
+          @conn.start
+          @conn
+        end
       rescue Bunny::TCPConnectionFailedForAllHosts => e
-        sleep(0.0001)
+        attempt += 1
+        sleep(0.1)
         retry
       rescue Bunny::TCPConnectionFailed => e
-        sleep(0.0001)
+        attempt += 1
+        sleep(0.1)
         retry
       end
     end
