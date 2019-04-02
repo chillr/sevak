03d5912565749a286a66e11134b383746bb8402b
Bibi D A <bibida.gec@gmail.com>
2019-04-02 11:47:51 +0530

    configuring multiple host (#12)
    configuring multiple host,  adding attempt counter, checking whether the connection is open, decreasing the retry time

--- lib/sevak/core.rb
+++ lib/sevak/core.rb
@@ -1,15 +1,26 @@
 module Sevak
   module Core
     def connection
-      return @conn if @conn
-      @conn ||= Bunny.new(
-        host: config.host,
-        port: config.port,
-        username: config.user,
-        password: config.password)
-
-      @conn.start
-      @conn
+      attempt = 0
+      begin
+        return @conn if (@conn && @conn.open?)
+        host = config.host.kind_of?(Array) ? config.host : [config.host]
+        @conn = Bunny.new(
+          hosts: host,
+          port: config.port,
+          username: config.user,
+          password: config.password)
+        @conn.start
+        @conn
+      rescue Bunny::TCPConnectionFailedForAllHosts => e
+        attempt += 1
+        sleep(0.001)
+        retry if attempt < 10
+      rescue Bunny::TCPConnectionFailed => e
+        attempt += 1
+        sleep(0.001)
+        retry if attempt < 10
+      end
     end
 
     def log(data)
