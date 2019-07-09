4ad576e84a69cf0c2c228a8207509f70de042ad2
RaphaelJose98 <44099159+RaphaelJose98@users.noreply.github.com>
2019-07-09 14:17:50 +0530

    Added exception handling for ConnectionClosedError (#13)
    * Added exception handling for ConnectionClosedError
    
    * handled exception at the publishing of message level
    
    * DRY code for publishing messages

--- lib/sevak/publisher.rb
+++ lib/sevak/publisher.rb
@@ -6,11 +6,11 @@ module Sevak
     include Singleton
 
     def self.publish(queue_name, message)
-      instance.queue(queue_name).publish(message.to_json)
+      publish_message(queue_name, message)
     end
 
     def self.delayed_publish(queue_name, message, delay = 0)
-      instance.publish_exchange(queue_name, message, delay.to_i)
+      publish_message(queue_name, message, delay.to_i)
     end
 
     def channel
@@ -29,5 +29,22 @@ module Sevak
       queue(queue_name).bind(exchange(queue_name))
       exchange(queue_name).publish(message.to_json, headers: { 'x-delay' => delay })
     end
+
+  private
+
+      def self.publish_message(queue_name, message, delay = nil)
+        attempt = 0
+        begin
+          if delay.nil? 
+            instance.queue(queue_name).publish(message.to_json)
+          else
+            instance.publish_exchange(queue_name, message, delay.to_i)
+          end
+        rescue Bunny::ConnectionClosedError => e
+          attempt += 1
+          sleep(0.001)
+          attempt < 2 ? retry : raise
+        end
+      end
   end
 end
